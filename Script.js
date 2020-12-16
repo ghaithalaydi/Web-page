406aaf2c2ff5051a1c203a48ffbf1a9170ef2ce2
ghaith alaydi <ghaith.alaydi@gmail.com>
2020-12-16 13:43:02 +0200

    returned all functionalities

--- Script.js
+++ Script.js
@@ -1,32 +1,32 @@
-// alert("Wlecome aboard");
-// var Clienttyp = prompt("Are you and organization or a business?");
+alert("Wlecome aboard");
+var Clienttyp = prompt("Are you and organization or a business?");
 
-// Clienttell(Clienttyp);
+Clienttell(Clienttyp);
 
-// function Clienttell(Clienttyp) {
-//     if (Clienttyp == 'organization') {
-//         alert("You will be redirected to Org solutions");
-//     }
-//     else if (Clienttyp == 'business') {
-//         alert("You will be redirected to business solutions");
-//     }
-//     else {
-//         alert("Kindly enter a matching answer organization or business owner");
-//     }
-// }
+function Clienttell(Clienttyp) {
+    if (Clienttyp == 'organization') {
+        alert("You will be redirected to Org solutions");
+    }
+    else if (Clienttyp == 'business') {
+        alert("You will be redirected to business solutions");
+    }
+    else {
+        alert("Kindly enter a matching answer organization or business owner");
+    }
+}
 
-// var age = prompt("Kindly enter your age");
+var age = prompt("Kindly enter your age");
 
-// function agerestriction(age) {
+function agerestriction(age) {
 
-//     if (age => 15) {
-//         alert("You are legally allowed to enter this website");
-//     }
-//     else {
-//         alert("kindly make sure to have a gaurdian by your side");
-//     }
-// }
-// agerestriction(age);
+    if (age => 15) {
+        alert("You are legally allowed to enter this website");
+    }
+    else {
+        alert("kindly make sure to have a gaurdian by your side");
+    }
+}
+agerestriction(age);
 
 var game = prompt("Want to play a quick game?");
 
