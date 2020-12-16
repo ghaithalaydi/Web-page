134f21c9ee79d250927ba61be57b3c6aa6e609ee
ghaith alaydi <ghaith.alaydi@gmail.com>
2020-12-16 13:38:37 +0200

    Added a new function

--- Script.js
+++ Script.js
@@ -1,29 +1,46 @@
-alert("Wlecome aboard");
-var Clienttyp = prompt("Are you and organization or a business?");
+// alert("Wlecome aboard");
+// var Clienttyp = prompt("Are you and organization or a business?");
 
-Clienttell(Clienttyp);
+// Clienttell(Clienttyp);
 
-function Clienttell(Clienttyp) {
-    if (Clienttyp == 'organization') {
-        alert("You will be redirected to Org solutions");
+// function Clienttell(Clienttyp) {
+//     if (Clienttyp == 'organization') {
+//         alert("You will be redirected to Org solutions");
+//     }
+//     else if (Clienttyp == 'business') {
+//         alert("You will be redirected to business solutions");
+//     }
+//     else {
+//         alert("Kindly enter a matching answer organization or business owner");
+//     }
+// }
+
+// var age = prompt("Kindly enter your age");
+
+// function agerestriction(age) {
+
+//     if (age => 15) {
+//         alert("You are legally allowed to enter this website");
+//     }
+//     else {
+//         alert("kindly make sure to have a gaurdian by your side");
+//     }
+// }
+// agerestriction(age);
+
+var game = prompt("Want to play a quick game?");
+
+function gameoutcome(game) {
+    if (game == "yes") {
+        alert("lets start");
+        var firstno = prompt("Kindly enter your first number");
+        var secondno = prompt("kindly enter your second number");
+        alert("you have entered " +firstno +" & " +secondno +" Tada!!");
     }
-    else if (Clienttyp == 'business') {
-        alert("You will be redirected to business solutions");
-    }
+    else if (game == "no") {
+        alert("maybe next time");}
     else {
-        alert("Kindly enter a matching answer organization or business owner");
+            alert("invalid input");
+        }
     }
-}
-
-var  age = prompt("Kindly enter your age");
-
-function agerestriction(age) {
-
-    if (age => 15) {
-        alert("You are legally allowed to enter this website");
-    }
-    else {
-        alert("kindly make sure to have a gaurdian by your side :)");
-    }
-}
-agerestriction(age);
+gameoutcome(game);
